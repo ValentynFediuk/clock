1b32de940281d48075186eab7c229057bea4f409
Valentyn <42549484+ValentynFediuk@users.noreply.github.com>
2021-11-16 15:31:32 +0200

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,40 @@
-$(function () {
-  $('body').css('background-color', 'blue')
-})
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+
+  let h = (currentDate.getHours() % 12).toString();
+  let m = currentDate.getMinutes().toString();
+  let s = currentDate.getSeconds().toString();
+
+  if (h.length < 2) {
+    h = '0' + h;
+  }
+  if (m.length < 2) {
+    m = '0' + m;
+  }
+  if (s.length < 2) {
+    s = '0' + s;
+  }
+
+  var clockString = h + ':' + m + ':' + s;
+  var colorString = '#' + h + m + s;
+
+  document.body.style.background = colorString;
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
